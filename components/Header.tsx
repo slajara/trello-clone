231d97f7cc30f030b213021e576e287e13624787
slajara <lajara.agullo@gmail.com>
2024-06-11 19:56:48 +0100

    chore: update header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -35,6 +35,13 @@ function Header() {
           <Avatar name="Samuel Lajara" round size="50" color="#0055D1" />
         </div>
       </div>
+
+      <div className="flex items-center justify-center px-5 md:py-5">
+        <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
+          <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is summarising your task for the day ...
+        </p>
+      </div>
     </header>
   );
 }
