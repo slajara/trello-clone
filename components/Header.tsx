88555f214b03828dab877e1cfad05a446d637788
slajara <lajara.agullo@gmail.com>
2024-06-11 20:06:05 +0100

    chore: update header component color

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,6 +8,23 @@ function Header() {
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div
+          className="
+            absolute
+            top-0
+            left-0
+            w-full
+            h-96
+            bg-gradient-to-br
+            from-pink-400
+            to-[#0055D1]
+            rounded-md
+            filter
+            blur-3xl
+            opacity-50
+            -z-50"
+        />
+
         <Image
           src="/trello-logo.png"
           alt="Trello Logo"
@@ -36,7 +53,7 @@ function Header() {
         </div>
       </div>
 
-      <div className="flex items-center justify-center px-5 md:py-5">
+      <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
           <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
           GPT is summarising your task for the day ...
